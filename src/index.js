7a07f3f679bb3cfde41faa7fafff07058ae80e30
Sandip Guchait <35764971+sandipguchait@users.noreply.github.com>
2019-03-06 17:52:36 +0530

    rendering songlist component

--- src/index.js
+++ src/index.js
@@ -1,6 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+
 import App from "./components/App";
+import reducers from "./reducers/index";
 
 const rootElement = document.getElementById("root");
-ReactDOM.render(<App />, rootElement);
+ReactDOM.render(
+	<Provider store={createStore(reducers)}>
+		<App />
+	</Provider>,
+	rootElement
+);
