22475b7dcf2e8147ab22482f0996761e487d1b1f
Sandip Guchait <35764971+sandipguchait@users.noreply.github.com>
2019-03-07 16:02:24 +0530

    Styling song detail component

--- src/components/SongDetail.js
+++ src/components/SongDetail.js
@@ -1,9 +1,20 @@
 import React from "react";
 import { connect } from "react-redux";
 
-const songDetail = props => {
-	console.log(props);
-	return <div>Song detail </div>;
+const songDetail = ({ selectedSong }) => {
+	if (!selectedSong) {
+		return <div>Select a Song</div>;
+	}
+	return (
+		<div style={{ marginTop: "20px" }}>
+			<h3>Details For: </h3>
+			<p>
+				Title: {selectedSong.song.title}
+				<br />
+				Duration: {selectedSong.song.duration}
+			</p>
+		</div>
+	);
 };
 
 const mapStateToProps = state => {
